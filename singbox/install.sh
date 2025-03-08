e227407bd936b465e3f3de7e1854713951e9c4a3
starry <115192496+sky22333@users.noreply.github.com>
2025-03-08 13:15:37 +0800

    Create install.sh

---
+++ singbox/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+# 版本号
+VERSION=1.11.4
+curl -Lo sing-box.deb "https://github.com/SagerNet/sing-box/releases/download/v${VERSION}/sing-box_${VERSION}_linux_amd64.deb"
+sudo dpkg -i sing-box.deb
+rm sing-box.deb
+
+echo "Sing-box ${VERSION} 安装成功!"
